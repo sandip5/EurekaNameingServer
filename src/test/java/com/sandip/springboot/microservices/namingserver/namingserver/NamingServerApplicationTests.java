44a78ba1079d5b78e3210652570a8a046b06e73a
sandip5 <singhsandeep.singh97@gmail.com>
2021-02-09 19:41:01 +0530

    ADDED- Eureka Naming Server Microservices

---
+++ src/test/java/com/sandip/springboot/microservices/namingserver/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandip.springboot.microservices.namingserver.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
